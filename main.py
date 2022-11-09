e940cc1a2ed91a5b7e929e0c656537a911bbb6cf
alexandrezurcher <60798271+alexandrezurcher@users.noreply.github.com>
2022-11-09 15:56:16 +0000

    writing the list to txt file

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
-
-response = requests.get("https://news.ycombinator.com")
+import lxml
+response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
 yc_web_page = response.text
 soup = BeautifulSoup(yc_web_page, "lxml")
-
 print(soup)
+
+title_tag = soup.find_all(name="h3", class_="title")
+
+movie_title_list = [title.getText() for title in title_tag]
+movie_title_list.reverse()
+print(movie_title_list)
+
+with open("movies.txt", "w") as f:
+    for movie in movie_title_list:
+        f.write(movie)
+        f.write("\n")
